242a4475fbdd242950dd0a97aaf274276a2c9198
wzl <wzl@wzldemacbook-pro.local>
2015-12-21 17:50:36 +0800

    项目初始化

---
+++ XWDataBaseManager/XWDataBaseManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XWDataBaseManager
+//
+//  Created by wzl on 15/12/21.
+//  Copyright © 2015年 wzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XWDataBaseManager/XWDataBaseManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XWDataBaseManager
+//
+//  Created by wzl on 15/12/21.
+//  Copyright © 2015年 wzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
